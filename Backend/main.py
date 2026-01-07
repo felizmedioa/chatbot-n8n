6404fa00aaf326d13ea94a843fa5718d8ad784ad
Jose Luis <luis_jose1978@hotmail.com>
2026-01-06 23:30:49 -0500

    Conexión integrada con n8n exitosa - Falta añadir estilos y que el chat tenga memoria

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI #Lo principal de fastapi
+from fastapi import FastAPI, HTTPException#Lo principal de fastapi
 from fastapi.middleware.cors import CORSMiddleware #Nos ayudará a configurar el CORS y los permisos
 from pydantic import BaseModel #Nos ayudará a definir facilmente como objetos el contenido de las peticiones, ademas de ayudar con las validaciones
+import httpx 
 
-app = FastAPI();
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
@@ -10,15 +11,48 @@ app.add_middleware(
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
-);
+)
 
 class SolicitudPrompt(BaseModel):
-    contenido: str;
+    contenido: str
 
 class RespuestaChat(BaseModel):
-    mensaje: str
-    auth: str
+    mensaje: str = "Texto por si falla el mensaje"
+    auth: str = "Texto por si falla el auth"
 
 @app.post("/realizar-pregunta")
-def recibir_Prompt(prompt: SolicitudPrompt):
-    return RespuestaChat(mensaje="Prompt Recibido con Exito", auth = "byJPuffs")
+
+async def consultarN8n(prompt: SolicitudPrompt):
+    n8n_url = "https://igts-jl.app.n8n.cloud/webhook-test/prompt-process"
+
+    envio = {
+        "texto": prompt.contenido,
+        "num": 1 
+    }
+
+    try:
+        async with httpx.AsyncClient() as client :
+
+            respuesta = await client.post(n8n_url, json = envio, timeout = 20.0)
+
+            print(respuesta.status_code)
+            print("Todo bien")
+            #print(respuesta.respuestaNino)
+            #print("Hasta acá está bien")
+
+            respuesta.raise_for_status()
+            #print("Hasta acá está bien")
+            msg = respuesta.json()
+            print("Si se pudo convertir la respuesta a json")
+            print(msg)
+
+            #respuestaUsuario = RespuestaChat(mensaje=msg["respuestaNino"], auth = "byJPuffs")
+
+            return RespuestaChat(mensaje=msg["respuestaNino"], auth = "byJPuffs")
+
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code = e.response.status_code, detail = "Los httpx.HTTPStatusError son errores durante el envio (n8n)")
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail = "Los Excpetion son errores en la conexión")
+    
+
